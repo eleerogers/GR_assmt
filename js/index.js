e97d010c781411715657af0844ac03438d4f239c
Lee Rogers <edwinleerogers@gmail.com>
2020-10-06 16:56:18 -0700

    add jquery to get star wars names from api

--- js/index.js
+++ js/index.js
@@ -6,4 +6,33 @@ $(document).ready(() => {
     $(this).closest('div.lifestyle-option').addClass('active').siblings().removeClass('active');
   })
 
+  let page = 1;
+  let characterObjs = [];
+  var totalChars;
+
+  $('.star-wars-btn').on('click', async () => {
+    const totalCharsUnd = typeof totalChars === 'undefined';
+    try {
+      if (totalCharsUnd || characterObjs.length < totalChars) {
+        var {data: {results, count}} = await axios(`https://swapi.dev/api/people/?page=${page}`);
+        if (totalCharsUnd) {
+          totalChars = count;
+        }
+        characterObjs = characterObjs.concat(results);
+        page += 1;
+      }
+    }
+    catch (err) {
+      console.error(err);
+    }
+    finally {
+      results.forEach(charObj => {
+        $('.star-wars-char-list').append(`<li>${charObj.name}</li>`);
+      })
+      if (characterObjs.length >= totalChars) {
+        $('.star-wars-btn').prop('disabled', true);
+      }
+    }
+  })
+
 })
